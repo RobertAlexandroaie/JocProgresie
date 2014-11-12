de9efd4c948e9ce06aea92379a3da17b7df0b3c7
Robert Alexandroaie <robert.alexandroaie@gmail.com>
2014-11-12 17:22:33 +0200

    Test added

---
+++ src/test/java/com/fii/homework/model/PlayerTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.fii.homework.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author ralexandroaie
+ *
+ */
+public class PlayerTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for {@link com.fii.homework.model.Player#addLetter(char)}.
+     */
+    @Test
+    public void testAddLetter() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link com.fii.homework.model.Player#isProgression(int, int)}.
+     */
+    @Test
+    public void testIsProgression() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link com.fii.homework.model.Player#isWinner(int)}.
+     */
+    @Test
+    public void testIsWinner() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link com.fii.homework.model.Player#getWord()}.
+     */
+    @Test
+    public void testGetWord() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link com.fii.homework.model.Player#getWinningWord()}.
+     */
+    @Test
+    public void testGetWinningWord() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
